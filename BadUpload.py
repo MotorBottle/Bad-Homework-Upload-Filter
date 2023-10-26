e42436559c2392a06f1955c5814694beb7f8633b
MotorBottle <71703952+MotorBottle@users.noreply.github.com>
2023-10-25 23:35:59 -0400

    V2.0 Added .tar recognition

--- BadUpload.py
+++ BadUpload.py
@@ -1,18 +1,38 @@
 import os
 import zipfile
 import shutil
+import tarfile
 
-directory = r'C:\Users\Techer\Document\Submissions' # Set your directory to process here
+directory = r"C:\Users\Ccrrq\Downloads\submissions" # Set your directory to process here
 keyword = 'hw3' # Set your keyword here
 # The script will find those upload files without putting them in folder (eg. name-date-hw3) as required
-
 class DirectoryProcessor:
     def __init__(self, directory, keyword):
         self.directory = directory
         self.extracted_dir = os.path.join(directory, 'extracted')
         self.keyword = keyword
 
+    def extract_tar_files(self):
+        bad_upload_dir = os.path.join(self.directory, 'BadUpload')
+        os.makedirs(bad_upload_dir, exist_ok=True)
+        for filename in os.listdir(self.directory):
+            if filename.endswith(('.tar', '.tar.gz')):
+                tar_path = os.path.join(self.directory, filename)
+                # Removing file extension to get folder name
+                folder_name = filename
+                for ext in ['.tar', '.tar.gz']:
+                    folder_name = folder_name.replace(ext, '')
+                # Adding 'NotZip-' prefix to the folder name
+                folder_name = 'NotZip-' + folder_name
+                extract_to_dir = os.path.join(bad_upload_dir, folder_name)
+                os.makedirs(extract_to_dir, exist_ok=True)
+                with tarfile.open(tar_path, 'r:*') as tar_ref:
+                    tar_ref.extractall(extract_to_dir)
+                    print(f'Extracted {filename} to {extract_to_dir}')
+
+
     def extract_zip_files(self):
+        self.extract_tar_files()
         os.makedirs(self.extracted_dir, exist_ok=True)
         for filename in os.listdir(self.directory):
             if filename.endswith('.zip'):
